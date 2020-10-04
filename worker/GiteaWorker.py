0acad81ed4954e53cff66f020d90dc1741e92ed0
BoYanZh <32470225+BoYanZh@users.noreply.github.com>
2020-10-05 03:06:11 +0800

    update: giteaworker minor fix

--- worker/GiteaWorker.py
+++ worker/GiteaWorker.py
@@ -6,10 +6,6 @@ class GiteaWorker():
     def __init__(self, args, baseUrl, orgName, token, hgroups,
                  logger=Logger()):
         self.args = args
-        # self.id_ = id_
-        # self.name = name
-        # self.projNum = projNum
-        # self.projScores = projScores
         self.logger = logger
         self.names = {
             item[1]: item[0]
@@ -22,13 +18,14 @@ class GiteaWorker():
 
     def raiseIssues(self, scores):
         for key, value in scores.items():
-            if not value.get('projComment'): continue
+            if not value.get('projComment'):
+                value['projComment'] = ['good job']
             id_ = self.names[key]
             repoName = getProjRepoName([id_, key, self.args.proj])
             url = f"{self.baseUrl}/repos/{self.orgName}/{repoName}/issues"
             data = {
                 "title": f"m{self.args.ms} feedback",
-                "body": '\n'.join(value.get('projComment')),
+                "body": '\n'.join(value['projComment']),
             }
             req = self.sess.post(url, data)
             self.logger.debug(f"{repoName} issue {req.status_code} {req.text}")
