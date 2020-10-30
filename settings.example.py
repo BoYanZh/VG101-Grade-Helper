e44199e698d7d5fa28c7fa510338543ee2ecff48
BoYanZh <bomingzh@sjtu.edu.cn>
2020-10-31 02:24:15 +0800

    update example settings

--- settings.example.py
+++ settings.example.py
@@ -1,28 +1,39 @@
 CANVAS_TOKEN = "asdfjkl;"
+COURSE_ID = 1903
+SID = "asdfjl;"
+MOSS_USER_ID = 000000
+JOJ_COURSE_ID = "vg101_fall_2020_manuel"
 GITEA_BASE_URL = "https://focs.ji.sjtu.edu.cn/git/api/v1"
 CANVAS_BASE_URL = "https://umjicanvas.com/api/v1"
-COURSE_ID = 1903
 ORG_NAME = "vg101"
-SID = "asdfjl;"
-JOJ_COURSE_ID = "vg101_fall_2020_manuel"
 PROCESS_COUNT = 16
 RUBRIC = {
-    "indvFailSubmit": [-0.5, 'individual submit absence, -0.5'],
-    "indvUntidy": [-0.25, 'individual branch untidy, -0.25'],
-    "groupFailSubmit": [-2.5, 'group submit absence, -2.5'],
-    "groupUntidy": [-0.25, 'group submit untidy, -0.25'],
-    "jojFailHomework": [-0.5, 'JOJ homework not passed, -0.5'],
-    "jojFailExercise": [-0.25, 'JOJ exercise not passed, -0.25'],
-    "jojFailCompile": [-1, 'JOJ fail to compile, -1'],
+    "indvFailSubmit": [-0.5, 'individual submit missing'],
+    "indvUntidy": [-0.25, 'individual branch untidy'],
+    "groupFailSubmit": [-2.5, 'group submit missing'],
+    "groupUntidy": [-0.25, 'group submit untidy'],
+    "jojFailHomework": [-0.5, 'JOJ homework not passed'],
+    "jojFailExercise": [-0.25, 'JOJ exercise not passed'],
+    "jojFailCompile": [-1, 'JOJ fail to compile'],
+    "indvLowCodeQuality": [-0.25, 'individual submit low code quality'],
+    "groupLowCodeQuality": [-0.25, 'group submit low code quality'],
+    "noReview": [-1, 'not review others\' code'],
 }
 JOJ_INFO = {
     "lang":
-    "matlab",
+    "c",
     "homeworkID":
-    "5f66161a91df0600062ff7aa",
+    "5f8533bb229b1b0006f11473",
     "problemInfo": [
-        ["ex2.m", "5f6614eb91df0600062ff7a7", 1],
-        ["ex5.m", "5f6624b791df0600062ff7da", 1],
-        ["ex6.m", "5f66324291df0600062ff834", 5],
+        [["ex1.c", "assignment.h"], "5f853289229b1b0006f1146a", 10],
+        [["ex2.c", "assignment.h"], "5f85330a229b1b0006f1146d", 10],
+        [[
+            "ex5.c", "prod.c", "prod.h", "sum.c", "sum.h", "exp.c", "exp.h",
+            "quorem.c", "quorem.h", "assignment.h"
+        ], "5f8539a4229b1b0006f11483", 10],
     ]
 }
+MANDATORY_FILES = []
+OPTIONAL_FILES = [
+    "ex3.c", "ex4.c", "ex1.h", "ex2.h", "ex3.h", "ex4.h", "ex5.h", "main.c"
+]
